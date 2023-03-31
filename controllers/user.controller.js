76fa53024727056ad51fea0c9d109c9aaa562e72
kdipto1 <kdipto@outlook.com>
2023-03-31 23:43:22 +0600

    reverse git commit

--- controllers/user.controller.js
+++ controllers/user.controller.js
@@ -1,58 +1,64 @@
 const filePath = process.cwd() + "/utils/data.json";
-// const data = require("../utils/data.json");
+const data = require("../utils/data.json");
 // import  data from "../utils/data.json";
-const JSONdb = require("simple-json-db");
-const dataS = new JSONdb(filePath);
+// const JSONdb = require("simple-json-db");
+// const dataS = new JSONdb(filePath);
 const fs = require("fs");
-const data = dataS.storage;
+// const data = dataS.storage;
+//
+// const StormDB = require("stormdb");
+// const engine = new StormDB.localFileEngine(filePath);
+// const db = new StormDB(engine);
+// const data = db.state;
 //
 
-module.exports.getRandomUser = async (req, res, next) => {
-  const randomIndex = await Math.floor(Math.random() * data.length);
-  const randomUser = await data[randomIndex];
+module.exports.getRandomUser = (req, res, next) => {
+  const randomIndex = Math.floor(Math.random() * data.length);
+  const randomUser = data[randomIndex];
   // console.log(data);
   res.json(randomUser);
 };
 
-module.exports.getAllUsers = async (req, res, next) => {
+module.exports.getAllUsers = (req, res, next) => {
   const limit = req.query.limit;
   if (limit) {
-    const limitedUsers = await data.slice(0, Number(limit));
+    const limitedUsers = data.slice(0, Number(limit));
     res.json(limitedUsers);
   } else {
     res.json(data);
   }
 };
 
-module.exports.saveAUser = async (req, res, next) => {
+module.exports.saveAUser = (req, res, next) => {
   const newUser = req.body;
+  console.log(newUser);
   const { gender, name, contact, address, photoUrl } = newUser;
-  if (gender && name && contact && address && photoUrl) {
-    const availableIds = await data.map((user) => user.id);
+  if (name && gender && address && contact && photoUrl) {
+    const availableIds = data.map((user) => user.id);
     let newUserId;
     do {
-      newUserId = await Math.floor(Math.random() * data.length + 1);
+      newUserId = Math.floor(Math.random() * data.length + 1);
     } while (availableIds.includes(newUserId));
-    const newUserWithId = await {
+    const newUserWithId = {
       id: newUserId,
       ...newUser,
     };
-    const newData = await [...data, newUserWithId];
+    const newData = [...data, newUserWithId];
     fs.writeFile(filePath, JSON.stringify(newData), (err) => {
       if (err) {
         throw err;
       }
+      res.send("save a user");
     });
-    res.send("save a user");
   }
 };
 
-module.exports.updateAUser = async (req, res, next) => {
+module.exports.updateAUser = (req, res, next) => {
   const id = req.query.id;
   const properties = req.body;
-  const user = await data.find((user) => user.id === Number(id));
+  const user = data.find((user) => user.id === Number(id));
   if (user) {
-    const updatedData = await data.map((user) => {
+    const updatedData = data.map((user) => {
       if (user.id === Number(id)) {
         return {
           ...user,
@@ -70,11 +76,11 @@ module.exports.updateAUser = async (req, res, next) => {
   }
 };
 
-module.exports.updateBulkUser = async (req, res, next) => {
+module.exports.updateBulkUser = (req, res, next) => {
   const userIdsToUpdate = req.body.userIds;
   const newDetails = req.body.newDetails;
   console.log(newDetails);
-  const updatedData = await data.map((user) => {
+  const updatedData = data.map((user) => {
     if (userIdsToUpdate.includes(user.id)) {
       return { ...user, ...newDetails };
     }
@@ -88,10 +94,10 @@ module.exports.updateBulkUser = async (req, res, next) => {
   });
 };
 
-module.exports.deleteAUser = async (req, res, next) => {
+module.exports.deleteAUser = (req, res, next) => {
   const id = req.params.id;
   if (id) {
-    const newData = await data.filter((user) => user.id !== Number(id));
+    const newData = data.filter((user) => user.id !== Number(id));
     fs.writeFile(filePath, JSON.stringify(newData), (err) => {
       if (err) {
         throw err;
